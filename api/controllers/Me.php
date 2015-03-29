dc743028ac936142c0338e840ea562d5de8376a6
Pablo de la Peña <p@shedcollective.org>
2015-03-29 17:38:56 +0100

    Added API endpoint for finding information about the currently logged in user

---
+++ api/controllers/Me.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Api\Auth;
+
+/**
+ * Returns information about the currently logged in user
+ *
+ * @package     Nails
+ * @subpackage  module-api
+ * @category    Controller
+ * @author      Nails Dev Team
+ * @link
+ */
+
+class Me extends \ApiController
+{
+    public static $requiresAuthentication = true;
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns basic details about the currently logged in user
+     * @return array
+     */
+    public function anyIndex()
+    {
+        return array(
+            'user' => array(
+                'id'        => activeUser('id'),
+                'firstName' => activeUser('first_name'),
+                'lastName'  => activeUser('last_name'),
+                'email'     => activeUser('email'),
+                'username'  => activeUser('username'),
+                'avatar'    => cdn_avatar(),
+                'gender'    => activeUser('gender')
+            )
+        );
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Get the current user's avatar
+     * @param  int   $width  The width of the avatar
+     * @param  int   $height The height of the avatar
+     * @return array
+     */
+    public function anyAvatar($width = null, $height = null)
+    {
+        $width  = $width ? $width : 100;
+        $height = $height ? $height : 100;
+
+        return array(
+            'url' => cdn_avatar(null, $width, $height)
+        );
+    }
+}
