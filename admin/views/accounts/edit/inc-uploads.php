822a08fcd39e4b69ec7074c1e9909f6ccbc08004
Pablo de la Peña <p@shedcollective.org>
2015-10-15 22:00:33 +0100

    Minor cosmetic tweaks

--- admin/views/accounts/edit/inc-uploads.php
+++ admin/views/accounts/edit/inc-uploads.php
@@ -5,49 +5,45 @@ if (isModuleEnabled('nailsapp/module-cdn')) {
     ?>
     <fieldset  id="edit-user-uploads" class="uploads">
         <legend><?=lang('accounts_edit_upload_legend')?></legend>
-        <p>
+        <ul>
         <?php
 
-            echo '<ul>';
+        if ($user_uploads) {
 
-            if ($user_uploads) {
+            foreach ($user_uploads as $file) {
 
-                foreach ($user_uploads as $file) {
+                echo '<li class="file">';
 
-                    echo '<li class="file">';
+                switch ($file->mime) {
 
-                    switch ($file->mime) {
+                    case 'image/jpg':
+                    case 'image/jpeg':
+                    case 'image/gif':
+                    case 'image/png':
 
-                        case 'image/jpg':
-                        case 'image/jpeg':
-                        case 'image/gif':
-                        case 'image/png':
+                        echo '<a href="' . cdnServe($file->id) . '" class="fancybox image">';
+                        echo img(cdnCrop($file->id, 35, 35));
+                        echo $file->filename_display;
+                        echo '<small>Bucket: ' . $file->bucket->slug . '</small>';
+                        echo '</a>';
+                        break;
 
-                            echo '<a href="' . cdnServe($file->id) . '" class="fancybox image">';
-                            echo img(cdnCrop($file->id, 35, 35));
-                            echo $file->filename_display;
-                            echo '<small>Bucket: ' . $file->bucket->slug . '</small>';
-                            echo '</a>';
-                            break;
+                    default:
 
-                        default:
-
-                            echo anchor(cdnServe($file->id) . '?dl=1', $file->filename_display . '<small>Bucket: ' . $file->bucket->slug . '</small>');
-                            break;
-                    }
-
-                    echo '</li>';
+                        echo anchor(cdnServe($file->id) . '?dl=1', $file->filename_display . '<small>Bucket: ' . $file->bucket->slug . '</small>');
+                        break;
                 }
 
-            } else {
-
-                echo '<li class="no-data">' . lang('accounts_edit_upload_nofile') . '</li>';
+                echo '</li>';
             }
 
-            echo '</ul>';
+        } else {
+
+            echo '<li class="no-data">' . lang('accounts_edit_upload_nofile') . '</li>';
+        }
 
         ?>
-        </p>
+        </ul>
     </fieldset>
     <?php
 
