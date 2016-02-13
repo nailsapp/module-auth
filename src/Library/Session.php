6f4e026afc6afa7a96764a98fcde53ec4d12b8b7
Pablo de la Peña <p@shedcollective.org>
2016-02-13 18:12:10 +0000

    using `isCli()` helper method

--- src/Library/Session.php
+++ src/Library/Session.php
@@ -37,7 +37,7 @@ class Session
      */
     public function __construct()
     {
-        if (php_sapi_name() !== 'cli') {
+        if (!isCli()) {
 
             /**
              * STOP! Before we load the session library, we need to check if we're using
