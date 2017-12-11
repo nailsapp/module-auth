b41bae67964c6c06a0154217846fc3d4926027e2
Pablo de la Peña <p@shedcollective.org>
2017-12-11 08:48:33 +0000

    Consolidted `keep_flashdata` method into mian Session library

--- src/Library/Session.php
+++ src/Library/Session.php
@@ -2,8 +2,8 @@
 
 /**
  * The class handles the user's session
- * @todo remove dependency on CodeIgniter's Session library
- * @todo properly handle CLI behaviour
+ * @todo        remove dependency on CodeIgniter's Session library
+ * @todo        properly handle CLI behaviour
  *
  * @package     Nails
  * @subpackage  module-auth
@@ -17,7 +17,7 @@ namespace Nails\Auth\Library;
 class Session
 {
     /**
-     * Whether on the cLI or not
+     * Whether on the CLI or not
      * @var boolean
      */
     private $bIsCli;
@@ -26,7 +26,7 @@ class Session
 
     /**
      * The session object
-     * @var \CI_DB_mysqli_driver
+     * @var \CI_Session
      */
     private $oSession;
 
@@ -49,7 +49,6 @@ class Session
             $sSessionTable = $oCi->config->item('sess_table_name');
 
             if ($sSessionTable === 'nails_session' && NAILS_DB_PREFIX !== 'nails_') {
-
                 $sSessionTable = str_replace('nails_', NAILS_DB_PREFIX, $sSessionTable);
                 $oCi->config->set_item('sess_table_name', $sSessionTable);
             }
@@ -58,7 +57,6 @@ class Session
             $this->bIsCli   = false;
 
         } else {
-
             $this->bIsCli = true;
         }
     }
@@ -67,18 +65,21 @@ class Session
 
     /**
      * Route calls to the CodeIgniter Session class
+     *
      * @param  string $sMethod    The method being called
      * @param  array  $aArguments Any arguments being passed
+     *
      * @return mixed
      */
     public function __call($sMethod, $aArguments)
     {
         if (!$this->bIsCli) {
-
-            return call_user_func_array(array($this->oSession, $sMethod), $aArguments);
-
+            if (method_exists($this, $sMethod)) {
+                return call_user_func_array([$this, $sMethod], $aArguments);
+            } else {
+                return call_user_func_array([$this->oSession, $sMethod], $aArguments);
+            }
         } else {
-
             return null;
         }
     }
@@ -87,17 +88,16 @@ class Session
 
     /**
      * Pass any property "gets" to the CodeIgniter Session class
+     *
      * @param  string $sProperty The property to get
+     *
      * @return mixed
      */
     public function __get($sProperty)
     {
         if (!$this->bIsCli) {
-
             return $this->oSession->{$sProperty};
-
         } else {
-
             return null;
         }
     }
@@ -106,14 +106,66 @@ class Session
 
     /**
      * Pass any property "sets" to the CodeIgniter Session class
+     *
      * @param  string $sProperty The property to set
      * @param  mixed  $mValue    The value to set
-     * @return mixed
+     *
+     * @return void
      */
     public function __set($sProperty, $mValue)
     {
         if (!$this->bIsCli) {
             $this->oSession->{$sProperty} = $mValue;
         }
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Keeps existing flashdata available to next request.
+     * http://codeigniter.com/forums/viewthread/104392/#917834
+     *
+     * @param  string|array $mKey The key to keep, null will retain all flashdata
+     *
+     * @return void
+     **/
+    public function keep_flashdata($mKey = null)
+    {
+        /**
+         * 'old' flashdata gets removed.  Here we mark all flashdata as 'new' to preserve
+         * it from _flashdata_sweep(). Note the function will NOT return FALSE if the $mKey
+         * provided cannot be found, it will retain ALL flashdata
+         */
+
+        if (is_null($mKey)) {
+
+            foreach ($this->oSession->userdata as $k => $v) {
+
+                $sOldFlashdataKey = $this->oSession->flashdata_key . ':old:';
+
+                if (strpos($k, $sOldFlashdataKey) !== false) {
+                    $sNewFlashdataKey = $this->oSession->flashdata_key . ':new:';
+                    $sNewFlashdataKey = str_replace($sOldFlashdataKey, $sNewFlashdataKey, $k);
+                    $this->oSession->set_userdata($sNewFlashdataKey, $v);
+                }
+            }
+
+            return;
+
+        } elseif (is_array($mKey)) {
+            foreach ($mKey as $k) {
+                $this->keep_flashdata($k);
+            }
+        }
+
+        // --------------------------------------------------------------------------
+
+        $sOldFlashdataKey = $this->oSession->flashdata_key . ':old:' . $mKey;
+        $value            = $this->oSession->userdata($sOldFlashdataKey);
+
+        // --------------------------------------------------------------------------
+
+        $sNewFlashdataKey = $this->oSession->flashdata_key . ':new:' . $mKey;
+        $this->oSession->set_userdata($sNewFlashdataKey, $value);
     }
 }
