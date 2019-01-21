573370a73d0ee37640e47e6fa15416d23a8743b5
Pablo de la Peña <p@shedcollective.org>
2019-01-21 16:27:57 +0000

    Increasing the size of the `username` columne

---
+++ migrations/9.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Migration:   9
+ * Started:     21/10/2019
+ * Finalised:   21/10/2019
+ *
+ * @package     Nails
+ * @subpackage  module-auth
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nails\ModuleAuth;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration9 extends Base
+{
+    /**
+     * Execute the migration
+     *
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}user` CHANGE `username` `username` VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;');
+    }
+}
