09abae85f4489116d2b9a15b140e1c0ca6b5bed7
Pablo de la Peña <p@shedcollective.org>
2019-06-10 12:49:14 +0100

    Refactored forgotten password controller

--- auth/controllers/PasswordForgotten.php
+++ auth/controllers/PasswordForgotten.php
@@ -15,9 +15,12 @@ use Nails\Auth\Controller\Base;
 use Nails\Auth\Model\Auth;
 use Nails\Auth\Model\User\Password;
 use Nails\Auth\Service\Session;
+use Nails\Common\Exception\NailsException;
+use Nails\Common\Exception\ValidationException;
 use Nails\Common\Service\Config;
 use Nails\Common\Service\FormValidation;
 use Nails\Common\Service\Input;
+use Nails\Email\Service\Emailer;
 use Nails\Factory;
 
 /**
@@ -31,10 +34,6 @@ class PasswordForgotten extends Base
     public function __construct()
     {
         parent::__construct();
-
-        // --------------------------------------------------------------------------
-
-        //  Specify a default title for this page
         $this->data['page']->title = lang('auth_title_forgotten_password');
     }
 
@@ -47,83 +46,81 @@ class PasswordForgotten extends Base
      **/
     public function index()
     {
-        //  If user is logged in they shouldn't be accessing this method
+        /** @var Session $oSession */
+        $oSession = Factory::service('Session', 'nails/module-auth');
         if (isLoggedIn()) {
-            /** @var Session $oSession */
-            $oSession = Factory::service('Session', 'nails/module-auth');
             $oSession->setFlashData('error', lang('auth_no_access_already_logged_in', activeUser('email')));
             redirect('/');
         }
 
-        //  If there's POST data attempt to validate the user
+        // --------------------------------------------------------------------------
+
         /** @var Input $oInput */
         $oInput = Factory::service('Input');
         if ($oInput->post() || $oInput->get('identifier')) {
 
-            //  Define vars
-            $_identifier = $oInput->post('identifier');
+            try {
 
-            /**
-             * Override with the $_GET variable if POST failed to return anything. Populate
-             * the $_POST var with some data so form validation continues as normal, feels
-             * hacky but works.
-             */
-
-            if (!$_identifier && $oInput->get('identifier')) {
-
-                $_POST['identifier'] = $oInput->get('identifier');
-                $_identifier         = $oInput->get('identifier');
-            }
-
-            // --------------------------------------------------------------------------
-
-            /**
-             * Set rules.
-             * The rules vary depending on what login method is enabled.
-             */
-
-            /** @var FormValidation $oFormValidation */
-            $oFormValidation = Factory::service('FormValidation');
-
-            switch (APP_NATIVE_LOGIN_USING) {
-
-                case 'EMAIL':
-                    $oFormValidation->set_rules('identifier', '', 'required|trim|valid_email');
-                    break;
-
-                case 'USERNAME':
-                    $oFormValidation->set_rules('identifier', '', 'required|trim');
-                    break;
-
-                default:
-                    $oFormValidation->set_rules('identifier', '', 'trim');
-                    break;
-            }
-
-            // --------------------------------------------------------------------------
-
-            //  Override default messages
-            $oFormValidation->set_message('required', lang('fv_required'));
-            $oFormValidation->set_message('valid_email', lang('fv_valid_email'));
-
-            // --------------------------------------------------------------------------
-
-            //  Run validation
-            if ($oFormValidation->run()) {
+                //  Define vars
+                $sIdentifier = $oInput->post('identifier');
 
                 /**
-                 * Some apps may want the forgotten password tool to always return as successful,
-                 * even if it wasn't. Bad UX, if you ask me, but I'm not the client.
+                 * Override with the $_GET variable if POST failed to return anything. Populate
+                 * the $_POST var with some data so form validation continues as normal, feels
+                 * hacky but works.
                  */
 
+                if (!$sIdentifier && $oInput->get('identifier')) {
+                    $_POST['identifier'] = $oInput->get('identifier');
+                    $sIdentifier         = $oInput->get('identifier');
+                }
+
+                // --------------------------------------------------------------------------
+
+                /**
+                 * Set rules.
+                 * The rules vary depending on what login method is enabled.
+                 */
+
+                /** @var FormValidation $oFormValidation */
+                $oFormValidation = Factory::service('FormValidation');
+
+                switch (APP_NATIVE_LOGIN_USING) {
+
+                    case 'EMAIL':
+                        $oFormValidation->set_rules('identifier', '', 'required|trim|valid_email');
+                        break;
+
+                    case 'USERNAME':
+                        $oFormValidation->set_rules('identifier', '', 'required|trim');
+                        break;
+
+                    default:
+                        $oFormValidation->set_rules('identifier', '', 'trim');
+                        break;
+                }
+
+                // --------------------------------------------------------------------------
+
+                //  Override default messages
+                $oFormValidation->set_message('required', lang('fv_required'));
+                $oFormValidation->set_message('valid_email', lang('fv_valid_email'));
+
+                // --------------------------------------------------------------------------
+
+                //  Run validation
+                if (!$oFormValidation->run()) {
+                    throw new ValidationException(lang('fv_there_were_errors'));
+                }
+
                 /** @var Config $oConfig */
-                $oConfig       = Factory::service('Config');
-                $alwaysSucceed = $oConfig->item('authForgottenPassAlwaysSucceed');
+                $oConfig        = Factory::service('Config');
+                $bAlwaysSucceed = $oConfig->item('authForgottenPassAlwaysSucceed');
 
                 //  Attempt to reset password
                 /** @var Password $oUserPasswordModel */
                 $oUserPasswordModel = Factory::model('UserPassword', 'nails/module-auth');
-                if ($oUserPasswordModel->setToken($_identifier)) {
+                if ($oUserPasswordModel->setToken($sIdentifier)) {
 
                     //  Send email to user
                     /** @var \Nails\Auth\Model\User $oUserModel */
@@ -131,104 +128,76 @@ class PasswordForgotten extends Base
                     switch (APP_NATIVE_LOGIN_USING) {
 
                         case 'EMAIL':
-                            $this->data['reset_user'] = $oUserModel->getByEmail($_identifier);
-
-                            //  User provided an email, send to that email
-                            $sendToEmail = $_identifier;
+                            $this->data['reset_user'] = $oUserModel->getByEmail($sIdentifier);
+                            $sSendToEmail             = $sIdentifier;
                             break;
 
                         case 'USERNAME':
-                            $this->data['reset_user'] = $oUserModel->getByUsername($_identifier);
-
-                            /**
-                             * Can't email a username, send to their ID and let the email library
-                             * handle the routing
-                             */
-
-                            $sendToId = $this->data['reset_user']->id;
+                            $this->data['reset_user'] = $oUserModel->getByUsername($sIdentifier);
+                            $sSendToId                = $this->data['reset_user']->id;
                             break;
 
                         default:
-                            if (valid_email($_identifier)) {
-
-                                $this->data['reset_user'] = $oUserModel->getByEmail($_identifier);
-
-                                //  User provided an email, send to that email
-                                $sendToEmail = $_identifier;
-
+                            if (valid_email($sIdentifier)) {
+                                $this->data['reset_user'] = $oUserModel->getByEmail($sIdentifier);
+                                $sSendToEmail             = $sIdentifier;
                             } else {
-
-                                $this->data['reset_user'] = $oUserModel->getByUsername($_identifier);
-
-                                /**
-                                 * Can't email a username, send to their ID and let the email library handle
-                                 * the routing
-                                 */
-
-                                $sendToId = $this->data['reset_user']->id;
+                                $this->data['reset_user'] = $oUserModel->getByUsername($sIdentifier);
+                                $sSendToId                = $this->data['reset_user']->id;
                             }
                             break;
                     }
 
                     // --------------------------------------------------------------------------
 
-                    if (!$alwaysSucceed && isset($sendToEmail) && !$sendToEmail) {
+                    if (!$bAlwaysSucceed && isset($sSendToEmail) && !$sSendToEmail) {
 
                         //  If we're expecting an email, and none is available then we're kinda stuck
-                        $this->data['error'] = lang('auth_forgot_email_fail_no_email');
+                        throw new NailsException(lang('auth_forgot_email_fail_no_email'));
 
-                    } elseif (!$alwaysSucceed && isset($sendToId) && !$sendToId) {
+                    } elseif (!$bAlwaysSucceed && isset($sSendToId) && !$sSendToId) {
 
                         //  If we're expecting an ID and it's empty then we're stuck again
-                        $this->data['error'] = lang('auth_forgot_email_fail_no_id');
+                        throw new NailsException(lang('auth_forgot_email_fail_no_id'));
 
-                    } elseif ($alwaysSucceed) {
+                    } elseif ($bAlwaysSucceed) {
 
-                        //  Failed, but we always succeed so, yeah, succeed
-                        $this->data['success'] = lang('auth_forgot_success');
+                        //  Failed, but configured to always succeed - nothing to do
 
                     } else {
 
                         //  We've got something, go go go
-                        $_data       = new stdClass();
-                        $_data->type = 'forgotten_password';
+                        $oEmail = (object) [
+                            'type' => 'forgotten_password',
+                        ];
 
-                        if (isset($sendToEmail) && $sendToEmail) {
-
-                            $_data->to_email = $sendToEmail;
-
-                        } elseif (isset($sendToId) && $sendToId) {
-
-                            $_data->to_id = $sendToId;
+                        if (isset($sSendToEmail) && $sSendToEmail) {
+                            $oEmail->to_email = $sSendToEmail;
+                        } elseif (isset($sSendToId) && $sSendToId) {
+                            $oEmail->to_id = $sSendToId;
                         }
 
                         // --------------------------------------------------------------------------
 
                         //  Add data for the email view
-                        $_code = explode(':', $this->data['reset_user']->forgotten_password_code);
-
-                        $_data->data             = new \stdClass();
-                        $_data->data->resetUrl   = site_url('auth/password/forgotten/' . $_code[1]);
-                        $_data->data->identifier = $_identifier;
+                        $aCode        = explode(':', $this->data['reset_user']->forgotten_password_code);
+                        $oEmail->data = (object) [
+                            'resetUrl'   => siteUrl('auth/password/forgotten/' . $aCode[1]),
+                            'identifier' => $sIdentifier,
+                        ];
 
                         // --------------------------------------------------------------------------
 
-                        //  Send user the password reset email
-                        if ($this->emailer->send($_data)) {
-
-                            $this->data['success'] = lang('auth_forgot_success');
-
-                        } elseif ($alwaysSucceed) {
-
-                            $this->data['success'] = lang('auth_forgot_success');
-
-                        } else {
-
-                            $this->data['error'] = lang('auth_forgot_email_fail');
+                        /** @var Emailer $oEmailer */
+                        $oEmailer = Factory::service('Emailer', 'nails/module-email');
+                        if (!$oEmailer->send($oEmail, true)) {
+                            if (!$bAlwaysSucceed) {
+                                throw new NailsException(lang('auth_forgot_email_fail'));
+                            }
                         }
                     }
 
-                } elseif ($alwaysSucceed) {
+                } elseif ($bAlwaysSucceed) {
 
                     $this->data['success'] = lang('auth_forgot_success');
 
@@ -237,26 +206,26 @@ class PasswordForgotten extends Base
                     switch (APP_NATIVE_LOGIN_USING) {
 
                         case 'EMAIL':
-                            $this->data['error'] = lang('auth_forgot_code_not_set_email', $_identifier);
+                            $sError = lang('auth_forgot_code_not_set_email', $sIdentifier);
                             break;
-
-                        // --------------------------------------------------------------------------
 
                         case 'USERNAME':
-                            $this->data['error'] = lang('auth_forgot_code_not_set_username', $_identifier);
+                            $sError = lang('auth_forgot_code_not_set_username', $sIdentifier);
                             break;
-
-                        // --------------------------------------------------------------------------
 
                         default:
-                            $this->data['error'] = lang('auth_forgot_code_not_set');
+                            $sError = lang('auth_forgot_code_not_set', $sIdentifier);
                             break;
                     }
+
+                    throw new NailsException($sError);
                 }
 
-            } else {
+                $oSession->setFlashData('success', lang('auth_forgot_success'));
+                redirect('auth/login');
 
-                $this->data['error'] = lang('fv_there_were_errors');
+            } catch (Exception $e) {
+                $this->data['error'] = $e->getMessage();
             }
         }
 
